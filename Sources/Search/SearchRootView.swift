8be78d688a9b2601837b629df611de42c78745bc
narumichikubo <narumichi7373@gmail.com>
2022-12-25 14:50:08 +0900

    add Search UI

--- Sources/Search/SearchRootView.swift
+++ Sources/Search/SearchRootView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 public struct SearchRootView: View {
+    @State var text: String = ""
+    
     public init () {}
     
     public var body: some View {
-        Text("SearchRootView")
+        VStack {
+            header
+            content
+            Spacer()
+        }
     }
+    
+    // ヘッダー
+    private var header: some View {
+        VStack {
+            HStack {
+                Text("Github User Search")
+                    .font(.title)
+                Spacer()
+            }
+            // 検索項目
+            TextField("user name", text: $text)
+                .onSubmit(onSubmitText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.asciiCapable)
+        }
+        .padding()
+
+    }
+    
+    /// コンテント
+    private var content: some View {
+        ScrollView {
+            VStack {
+                // 検索結果リスト
+                Text("検索ワードに一致するユーザーが存在しません。")
+            }
+        }
+    }
+}
+
+
+/// UI Events
+extension SearchRootView {
+    func onSubmitText() {}
+    
 }
 
 public struct SearchRootView_Previews: PreviewProvider {
