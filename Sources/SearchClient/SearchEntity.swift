799acd867f94f398f8a35afa2a0ad5af1e3fb928
narumichikubo <narumichi7373@gmail.com>
2023-01-04 18:45:07 +0900

    add SearchEntity

---
+++ Sources/SearchClient/SearchEntity.swift
@@ -0,0 +1,63 @@
+//
+//  SearchEntity.swift
+//  
+//
+//  Created by Narumichi Kubo on 2023/01/03.
+//
+
+import Foundation
+
+public enum SearchEntity {
+    
+    public struct Request: Encodable, Equatable {
+        var q: String
+        
+        public init(q: String) {
+            self.q = q
+        }
+    }
+    
+    public enum Response {
+        public struct Users: Decodable, Equatable {
+            var totalCount: Int
+            var incompleteResults: Bool
+            var items: [User]
+            
+            public static let mockUsers = Users(
+                totalCount: 2,
+                incompleteResults: false,
+                items: [
+                    User(
+                        login: "narumichi0710",
+                        avatarUrl: "https://avatars.githubusercontent.com/u/65114797?v=4",
+                        reposUrl: "https://api.github.com/users/narumichi0710/repos"
+                    ),
+                    User(
+                        login: "narumichi0710",
+                        avatarUrl: "https://avatars.githubusercontent.com/u/65114797?v=4",
+                        reposUrl: "https://api.github.com/users/narumichi0710/repos"
+                    )
+                ]
+            )
+            
+            private enum CodingKeys: String, CodingKey {
+                case totalCount = "total_count"
+                case incompleteResults = "incomplete_results"
+                case items
+            }
+        }
+        
+        public struct User: Decodable, Equatable, Identifiable {
+            public var id = UUID()
+            var login: String
+            var avatarUrl: String
+            var reposUrl: String
+            
+            private enum CodingKeys: String, CodingKey {
+                case login
+                case avatarUrl = "avatar_url"
+                case reposUrl = "repos_url"
+            }
+        }
+    }
+}
